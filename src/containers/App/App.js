9f424d94c39d576e45af68acb04ebe8f4f7595d4
jarek <jarek.mokrzycki@gmai.com>
2018-05-21 23:51:47 +0200

    added flickity, updated readme

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './../../flickity/dist/flickity.min.css';
+import flickity from './../../flickity/dist/flickity.pkgd.min.js';
 
 export default class App extends React.Component {
+
   render(){
     return(
-      <div>main</div>
+        <div>
+          <div class="main-gallery" data-flickity-options='{ "cellAlign": "left", "contain": true }'>
+            <div class="gallery-cell">...</div>
+            <div class="gallery-cell">...</div>
+            <div class="gallery-cell">...</div>
+          </div>
+        </div>
     )
   }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '.styles/index.css';
+import './styles/index.css';
 import App from './containers/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
